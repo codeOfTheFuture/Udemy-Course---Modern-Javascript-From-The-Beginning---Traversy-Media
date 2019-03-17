8545ae160e4196f01fc224542cbebf5d88cb3761
Jeff Oliver <jeffoliver3384@gmail.com>
2019-03-17 17:20:22 -0400

    Regular Expressions [Part 2] - Metacharacter Symbols

---
+++ Regular Expressions [Part 2] - Metacharacter Symbols/app.js
@@ -0,0 +1,32 @@
+let re;
+re = /hello/;
+re = /hello/i;
+
+// Metacharacter symbols
+re = /^h/i;       // Must start with
+re = /world$/i;  // Must end with
+re = /^hello$/i;   // Must begin and end with
+re = /h.llo/i;   // Matches any ONE character
+re = /h*llo/i;   // Matches any character 0 or more times
+re = /gre?a?y/i; // Optional character
+re = /gre?a?y\?/i; // Escape character
+
+
+// String to match
+const str = 'gray?';
+
+// Log results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str) {
+  if(re.test(str)) {
+    console.log(`${str}
+    matches ${re.source}`);
+  } else {
+    console.log(`${str}
+    does NOT match ${re.source}`);
+  }
+}
+
+reTest(re, str);
